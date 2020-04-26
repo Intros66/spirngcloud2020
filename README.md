8b81d2086c88d1c95b43f5d7531b4017db951728
Intros66 <small_wn@163.com>
2020-04-26 20:44:58 +0800

    Rest微服务工程搭建

---
+++ README.md
@@ -0,0 +1,2 @@
+## SpirngCloud2020
+学习尚硅谷周阳老师SpringCloud(H版&alibaba)的学习笔记、源码
---
+++ cloud-provider-payment8001/src/main/java/com/ssp/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,27 @@
+package com.ssp.springcloud.service.impl;
+
+import com.ssp.springcloud.dao.PaymentDao;
+import com.ssp.springcloud.entities.Payment;
+import com.ssp.springcloud.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+//    @Autowired
+    @Resource
+    private PaymentDao paymentDao;
+
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+}
