fead4acd158145171d89ed8a87eac618ab027997
Intros66 <small_wn@163.com>
2020-05-02 23:28:47 +0800

    OpenFeign服务调用

--- cloud-consumer-feign-order80/src/main/java/com/ssp/cloud/service/PaymentFeignService.java
+++ cloud-consumer-feign-order80/src/main/java/com/ssp/cloud/service/PaymentFeignService.java
@@ -3,11 +3,10 @@ package com.ssp.cloud.service;
 import com.ssp.springcloud.entities.CommonResult;
 import com.ssp.springcloud.entities.Payment;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@Component
+
 @FeignClient(value = "CLOUD-PAYMENT-SERVICE")
 public interface PaymentFeignService {
 
